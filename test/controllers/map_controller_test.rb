8c133428e00c1725066b065acc2cabc3906a39a6
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2015-05-06 07:54:16 +0300

    Install cucumber and capybara for testing

--- test/controllers/map_controller_test.rb
+++ test/controllers/map_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class MapControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
+    assert_select 'h1', 'Мои карты'
   end
 
+  test 'should get create page' do
+    get :create
+    assert_response :success
+    assert_select 'h1', 'Добавление карты'
+    assert '[name="map_name"]'
+    assert_empty '[name="map_name"][value=?]', 'Map name should be empty in create form'
+  end
 end
